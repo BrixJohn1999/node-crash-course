45f1bf7f9664c561241efa04e96c88839c5fda8a
Brix John Novesteras <brixjohnovesteras@gmail.com>
2024-01-18 21:37:30 +0800

    global read and write file

--- files.js
+++ files.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 
-
 // reading files
 // fs.readFile('./docs/blog1.txt', (err, data) => {
 //     if(err){
@@ -10,15 +9,34 @@ const fs = require("fs");
 // })
 // console.log('last line')
 
-
 // writing files
-fs.writeFile('./docs/blog1.txt', 'helloWorld 123', () => {
-    console.log('file was written')
-
-})
+// fs.writeFile("./docs/blog1.txt", "helloWorld 123", () => {
+//   console.log("file was written");
+// });
 
 // directories
+if (!fs.existsSync("./assets")) {
+  fs.mkdir("./assets", (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("folder created");
+  });
+} else {
+  fs.rmdir("./assets", (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("folder deleted");
+  });
+}
 
 // deleting files
-
-https://youtu.be/OIBIXYLJjsI?list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&t=1471
+if (fs.existsSync("./docs/deleteme.txt")) {
+  fs.unlink("./docs/deleteme.txt", (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("file deleted");
+  });
+}
