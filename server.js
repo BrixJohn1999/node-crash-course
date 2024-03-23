af0e7e6af0a3f493773c9d759a13c693a15a1ce3
brix john novesteras <brixjohnovesteras@gmail.com>
2024-03-23 10:52:37 +0800

    update

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("request made");
+  console.log(req);
 });
-server.listen(3000, "localhost", ()=> {
-    console.log("listening for request on port 3000")
+server.listen(3000, "localhost", () => {
+  console.log("listening for request on port 3000");
 });
-
-https://youtu.be/DQD00NAUPNk?list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&t=3
